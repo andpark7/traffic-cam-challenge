c986eab9afc3f1d30768e95c4d361555a76c4c49
Andre Park <andre_5991@yahoo.com>
2014-11-23 17:32:47 -0800

    changes to traffic cam

--- js/app.js
+++ js/app.js
@@ -11,3 +11,54 @@
 //you should also write the code to filter the set of markers when the user
 //types a search phrase into the search box
 
+$(document).ready(function() {
+    var mapElem = document.getElementById('map');
+    var center = {
+        lat: 47.6,
+        lng: -122.3
+    };
+
+    var map = new google.maps.Map(mapElem, {
+        center: center,
+        zoom: 12
+    });
+
+    var infoWindow = new google.maps.InfoWindow();
+
+    $.getJSON('http://data.seattle.gov/resource/65fc-btcc.json')
+        .done(function(data) {
+            data.forEach(function (station) {
+                var marker = new google.maps.Marker({
+                    position: {
+                        lat: Number(station.location.latitude),
+                        lng: Number(station.location.longitude)
+                    },
+                    map: map
+                });
+
+                google.maps.event.addListener(marker, 'click', function() {
+                    var html = '<p>' + station.cameralabel + '</p>';
+                    html += '<img src=' + station.imageurl.url + '>';
+                    map.panTo(this.getPosition());
+                    infoWindow.setContent(html);
+                    infoWindow.open(map, this);
+                });
+
+                $('#search').bind('search keyup', function() {
+                    var searchElem = $(this).text().toLowerCase();
+                    if (marker.text().toLowerCase().indexOf(searchElem) == -1) {
+                        marker.setMap(null);
+                    } else {
+                        marker.setMap(map);
+                    }
+                });
+
+            });
+        })
+        .fail(function(error) {
+            alert("There is an error!");
+        });
+
+});
+
+
